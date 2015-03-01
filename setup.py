5a41c8affc136ec611b01657f1e5c6c1d9a3e71a
Jan Gosmann <jan@hyper-world.de>
2015-03-01 17:06:51 -0500

    Prepare PyPI distribution.

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 
-setup(name='pylint-venv', version='1.0', py_modules=['pylint_venv'])
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='pylint-venv',
+    version='1.0',
+    description='pylint-venv provides a Pylint init-hook to use the same '
+    'Pylint installation with different virtual environments.',
+    long_description=long_description,
+    author='Jan Gosmann',
+    author_email='jan@hyper-world.de',
+    url='https://github.com/jgosmann/pylint-venv/',
+    py_modules=['pylint_venv'],
+    provides=['pylint_venv'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ]
+)
