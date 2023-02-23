cbb9571444a638713267da64d4a30731faded109
Michał Górny <mgorny@gentoo.org>
2023-02-23 21:27:02 +0100

    Make the automated test work without which(1) (#15)
    which(1) is a third-party executable that's not installed on every
    system out there, and it is entirely redundant to shell builtins such
    as `command -v` (POSIX sh) or `type -P` (bash).  Since there does not
    seem to be any real need to use full path here, just inline the `pylint`
    call.

--- test/test.sh
+++ test/test.sh
@@ -3,10 +3,9 @@
 set -o errexit -o nounset -o pipefail -o xtrace
 
 WORKDIR=$(dirname "$0")
-PYLINT_CMD=$(which pylint)
 VENV_DIR=$(mktemp -d)
 
 python3 -m venv "$VENV_DIR"
 source "$VENV_DIR/bin/activate"
-"$PYLINT_CMD" --rcfile "$WORKDIR/pylintrc" "$WORKDIR/empty.py" 2>&1 | tee "$VENV_DIR/pylint.log" | grep "^Using venv: $VENV_DIR\$"
+pylint --rcfile "$WORKDIR/pylintrc" "$WORKDIR/empty.py" 2>&1 | tee "$VENV_DIR/pylint.log" | grep "^Using venv: $VENV_DIR\$"
 rm -r "$VENV_DIR"
