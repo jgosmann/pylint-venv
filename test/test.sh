b8bf64cf196ea400bda74d7c96b56e342182925e
Jan Gosmann <jan@hyper-world.de>
2023-10-24 20:16:30 +0200

    Add pyproject.toml configuration
    Addresses #17.

--- test/test.sh
+++ test/test.sh
@@ -8,4 +8,5 @@ VENV_DIR=$(mktemp -d)
 python3 -m venv "$VENV_DIR"
 source "$VENV_DIR/bin/activate"
 pylint --rcfile "$WORKDIR/pylintrc" "$WORKDIR/empty.py" 2>&1 | tee "$VENV_DIR/pylint.log" | grep "^Using venv: $VENV_DIR\$"
+pylint --rcfile "$WORKDIR/pyproject.toml" "$WORKDIR/empty.py" 2>&1 | tee "$VENV_DIR/pylint.log" | grep "^Using venv: $VENV_DIR\$"
 rm -r "$VENV_DIR"
